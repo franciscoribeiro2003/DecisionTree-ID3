8b3befcf40e537ae4511fd1fb8628dfaa0cbb959
MatheusBissacot <59031323+MatheusBissacot@users.noreply.github.com>
2023-05-26 10:34:06 +0100

    Update decisiontree.py

--- decisiontree.py
+++ decisiontree.py
@@ -1,5 +1,5 @@
+import math
 import numpy as np
-from collections import Counter
 
 class Node:
     def __init__(self):
@@ -8,55 +8,87 @@ class Node:
         self.isLeaf = False
         self.pred = ""
 
-def ID3(examples, target_attribute, attributes):
+
+def entropy(examples, classX):
+    pos = 0.0
+    neg = 0.0
+    for _, row in examples.iterrows():
+        if row[classX] == "Yes" or row[classX] == "yes":
+            pos += 1
+        else:
+            neg += 1
+    if pos == 0.0 or neg == 0.0:
+        return 0.0
+    else:
+        p = pos / (pos + neg)
+        n = neg / (pos + neg)
+        return -(p * math.log(p, 2) + n * math.log(n, 2))
+    
+def info_gain(examples, attr, classX):
+    uniq = np.unique(examples[attr].astype(str))
+    #print ("\n",uniq)
+    gain = entropy(examples, classX)
+    #print ("\n",gain)
+
+    for u in uniq:
+        subdata = examples[examples[attr] == u]
+        #print ("\n",subdata)
+        sub_e = entropy(subdata, classX)
+        gain -= (float(len(subdata)) / float(len(examples))) * sub_e
+        #print ("\n",gain)
+    return gain
+
+def ID3(examples, attrs, classX):
     root = Node()
 
-    # Check if all examples are positive
-    if all(row[target_attribute] == '+' for row in examples):
-        root.label = '+'
-        return root
-
-    # Check if all examples are negative
-    if all(row[target_attribute] == '-' for row in examples):
-        root.label = '-'
-        return root
-
-    # Check if no predicting attributes are left
-    if not attributes:
-        root.label = get_most_common_label(examples, target_attribute)
-        return root
-
-    best_attr = get_best_attribute(examples, target_attribute, attributes)
-    root.attribute = best_attr
-
-    for value in get_unique_values(examples, best_attr):
-        subset_examples = [row for row in examples if row[best_attr] == value]
-
-        if not subset_examples:
-            leaf = Node(label=get_most_common_label(examples, target_attribute))
-            root.add_child(value, leaf)
+    max_gain = 0
+    max_feat = ""
+    for feature in attrs:
+        #print ("\n",examples)
+        gain = info_gain(examples, feature, classX)
+        if gain > max_gain:
+            max_gain = gain
+            max_feat = feature
+    root.value = max_feat
+    #print ("\nMax feature attr",max_feat)
+    uniq = np.unique(examples[max_feat].astype(str))
+    #print ("\n",uniq)
+    for u in uniq:
+        #print ("\n",u)
+        subdata = examples[examples[max_feat].astype(str) == u]
+        #print ("\n",subdata)
+        if entropy(subdata, classX) == 0.0:
+            newNode = Node()
+            newNode.isLeaf = True
+            newNode.value = u
+            newNode.pred = np.unique(subdata[classX])
+            root.children.append(newNode)
         else:
-            new_attributes = attributes.copy()
-            new_attributes.remove(best_attr)
-            subtree = ID3(subset_examples, target_attribute, new_attributes)
-            root.add_child(value, subtree)
+            dummyNode = Node()
+            dummyNode.value = u
+            new_attrs = attrs.copy()
+            new_attrs.remove(max_feat)
+            child = ID3(subdata, new_attrs, classX)
+            dummyNode.children.append(child)
+            root.children.append(dummyNode)
 
     return root
 
-def get_most_common_label(examples, target_attribute):
-    labels = [row[target_attribute] for row in examples]
-    label_counts = Counter(labels)
-    most_common_label = label_counts.most_common(1)[0][0]
-    return most_common_label
+def printTree(root: Node, depth=0):
+    for i in range(depth):
+        print("\t", end="")
+    print(root.value, end="")
+    if root.isLeaf:
+        print(" -> ", root.pred)
+    print()
+    for child in root.children:
+        printTree(child, depth + 1)
 
-def get_best_attribute(examples, target_attribute, attributes):
-    # Implement the logic to select the best attribute based on a suitable criterion
-    # For example, you can use information gain or Gini index
-    # Here, we'll assume a simple random selection
-    return attributes[0]
-
-def get_unique_values(examples, attribute):
-    values = set()
-    for row in examples:
-        values.add(row[attribute])
-    return values
+def classify(root: Node, new):
+    for child in root.children:
+        if child.value == new[root.value]:
+            if child.isLeaf:
+                print ("Predicted Label for new example", new," is:", child.pred)
+                exit
+            else:
+                classify (child.children[0], new)
