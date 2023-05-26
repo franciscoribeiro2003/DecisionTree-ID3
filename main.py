dc02adcf29dc637aacfda3e9c7fb418a35c176ca
MatheusBissacot <59031323+MatheusBissacot@users.noreply.github.com>
2023-05-26 10:34:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
-import csv
-from sys import argv
+import pandas as pd
 import decisiontree as dt
+from sys import argv
 
-def read_csv(filename):
-    data = []
-    with open(filename, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
-    return data
+examples = pd.read_csv(argv[1])
+attributes = [attr for attr in examples]
+classX = attributes[-1]
+attributes.pop(0)
+attributes.pop(-1)
 
-examples = read_csv(argv[1])
-
-# Extract attribute names from the column headers
-attributes = list(examples[0].keys())[1:-1]
-
-# Extract target attribute name
-target_attribute = list(examples[0].keys())[-1]
-
-tree = dt.ID3(examples, attributes, target_attribute)
+tree = dt.ID3(examples, attributes, classX)
+dt.printTree(tree)
